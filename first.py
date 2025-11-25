f6734c9631a5bcb537f144ef0d5ce5a7dc61d4da
Idrisb53 <idrisbadshah421@gmail.com>
2025-11-25 13:34:09 +0530

    Python Code

---
+++ first.py
@@ -0,0 +1,7 @@
+#write a program that  prints out the numbers of second in a year
+seconds_in_a_minute = 60
+minutes_in_an_hour = 60
+hours_in_a_day = 24
+days_in_a_year = 365
+seconds_in_a_year = seconds_in_a_minute * minutes_in_an_hour * hours_in_a_day * days_in_a_year
+print(seconds_in_a_year)
