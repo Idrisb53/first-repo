035b28f4c1a44ea6d9a3270f66236f5e4093a026
Idrisb53 <idrisbadshah421@gmail.com>
2025-11-25 13:56:55 +0530

    Python Code

--- first.py
+++ first.py
@@ -5,3 +5,12 @@ hours_in_a_day = 24
 days_in_a_year = 365
 seconds_in_a_year = seconds_in_a_minute * minutes_in_an_hour * hours_in_a_day * days_in_a_year
 print(seconds_in_a_year)
+
+# write a function to check prime number
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True 
---
+++ prime.py
@@ -0,0 +1,16 @@
+# write a function to check prime number
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True      
+# Example usage
+if __name__ == "__main__":
+    test_numbers = [1, 2, 3, 4, 5, 16, 17, 18, 19, 20]
+    for number in test_numbers:
+        if is_prime(number):
+            print(f"{number} is a prime number.")
+        else:
+            print(f"{number} is not a prime number.")
